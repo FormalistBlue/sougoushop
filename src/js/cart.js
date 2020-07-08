a9cdfb24aedf7cc6ad41eb3aeb3731a2eaaf1a06
FormalistBlue <529206445@qq.com>
2020-07-08 12:00:03 +0800

    feat:change goodCount

--- src/js/cart.js
+++ src/js/cart.js
@@ -8,9 +8,10 @@ require(["/js/config.js"], () => {
                 let info = JSON.parse(localStorage.getItem("info"));
                 $("#tbody").html(template("showdata", { info }));
                 this.deleteData(info);
+                this.changeCount(info);
             }
             deleteData(info) {
-                let _this=this;
+                let _this = this;
                 $(".goodlist").on("click", ".delete", function () {
                     let id = $(this).parents(".tr").attr("id");
                     info.map((item, index) => {
@@ -22,6 +23,23 @@ require(["/js/config.js"], () => {
                     localStorage.setItem("info", JSON.stringify(info));
                     console.log(this);
                     _this.showData();
+                });
+            }
+            changeCount(info) {
+                let _this = this;
+                $(".goodlist").on("input", ".inputCount", function () {
+                    let id = $(this).parents(".tr").attr("id");
+                    info.map((item, index) => {
+                        if (id == item.id) {
+                            let number = $(".inputCount")[index].value;
+                            number ? number : 1;
+                            // console.log($('.inputCount')[index].value);
+                            item.count = number;
+                            console.log(info);
+                        }
+                    });
+                    localStorage.setItem("info", JSON.stringify(info));
+                    console.log(this);
                 });
             }
         }
